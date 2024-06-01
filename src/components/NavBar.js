bbc82d26c1cc2f9e3f9bf4484bed488e0065ed5a
Tumaini <tumainikebaya.234@gmail.com>
2024-06-01 18:51:00 +0300

    Adds projects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+    <a id="home" href="#home">home</a>
+    <a id="about" href="#about">about</a>
+    <a id="projects" href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
